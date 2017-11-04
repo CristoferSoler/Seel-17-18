100d12e70473846492e1c2318b9bc39e0e1248b3
Peter <elMudo92@win.tu-berlin.de>
2017-11-04 18:18:20 +0100

    Remote database in use now!

--- website/website/settings.py
+++ website/website/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fordjango',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '18.194.158.20',
+        'PORT': '5432',
     }
 }
 
