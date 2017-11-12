3c8972cfee5e9e8fd9068920d08715ed7cfbcf4c
Rima Faiqoh Augustine <augustine@campus.tu-berlin.de>
2017-11-12 11:22:22 +0100

    re-upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+
+   <app-navbar></app-navbar>
+    
+
+  <router-view></router-view>
+
+    <app-footer></app-footer>
+ 
+   </div>
+</template>
+
+<script>
+export default {
+  
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+
+<style lang="sass">
+  @import "../node_modules/bulma/bulma.sass";
+ </style>
+ 
+ <style>
+
+ </style>
+ 
+
