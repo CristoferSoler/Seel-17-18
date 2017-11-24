310fc4bef275f7bf2566b6a7f2c655f32df3a14d
pkabus <peter.kabus@gmx.de>
2017-11-24 15:27:56 +0100

    Basic importer script added which does simply put all content below the root article

--- django-wiki/Scripts/importer.py
+++ django-wiki/Scripts/importer.py
@@ -1,12 +1,14 @@
 import os
 import django
+import json
 # import psycopg2
 # from config import config
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bsi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bsiwiki.settings")
 django.setup()
+
+from wiki.models import Article, URLPath, Site, ArticleRevision
 # from website import model
-from bsi.models import BSIArticle
 
 def BSI_db_import(folder_location):
     BSI_description = []
@@ -43,18 +45,60 @@ def BSI_db_import(folder_location):
 
 
 def insert_BSI_db():
-    BSI_description = BSI_db_import(r'C:\Users\Peter\projects\Seel-17-18\programming\bsiCrawler\content')
+    # BSI_description = BSI_db_import(r'C:\Users\Peter\projects\Seel-17-18\programming\bsiCrawler\content')
 
-    for i in range(0, BSI_description.__len__()):
-        id = BSI_description[i][0]
-        title = BSI_description[i][1]
-        location = BSI_description[i][2]
-        article_type = BSI_description[i][3]
-        BSIArticle.objects.create(id=id, title=title, location=location, article_type=article_type)
+    for dirpath, dirnames, filenames in os.walk(r'C:\Users\Peter\projects\Seel-17-18\programming\bsiCrawler\content'):
+        for filename in [f for f in filenames if f.endswith(".json")]:
+            # get the drive and the filepath
+            # drive, path_and_file = os.path.splitdrive(os.path.join(dirpath, filename))
+            path_and_file = os.path.join(dirpath, filename)
+            # get the path and file name
+            location, file = os.path.split(path_and_file)
+            # get the type of the file
+            # if os.path.basename(location) == "C_MEASURE":
+            #     article_type = 'CM'
+            # elif os.path.basename(location) == "COMPONENT":
+            #     article_type = 'C'
+            # elif os.path.basename(location) == "THREAT":
+            #     article_type = 'T'
+            # if filename.startswith("B"):
+            #     article_type = 'C'
+            # else:
+            #     article_type = 'C'
+                # else:  action_type = 'G'
+                # get the file id and the titel
+            file_name = os.path.splitext(file)[0]
+            id = "".join(file_name.split(" ", 2)[:2])
+           # title = file_name.split(" ", 2)[2]
 
+            # read json file content in variable
+            with open(path_and_file) as data_file:
+                content = json.load(data_file)
+
+
+            revision_kwargs = {'content': content, 'user_message': 'Crawler input', 'ip_address': 'None'}
+            parent = URLPath.objects.filter(slug=None)[0]
+            slug = id
+
+            site = Site.objects.get_current()
+            create_article_with_revision(site=site, parent=parent, slug=id, content=content, rev_title=file_name, rev_kwargs=revision_kwargs)
+
+
+def create_article_with_revision(site, parent, slug, content, rev_title, rev_kwargs):
+    article_kwargs = {}
+    article = Article(**article_kwargs)
+    article.add_revision(ArticleRevision(title=rev_title, **rev_kwargs),
+                         save=True)
+    article.save()
+    newpath = URLPath.objects.create(
+        site=site,
+        parent=parent,
+        slug=slug,
+        article=article)
+    article.add_object_relation(newpath)
 
 if __name__ == '__main__':
-    BSIArticle.objects.all().delete()
+    # Article.objects.all().delete()
     insert_BSI_db()
     # finally:
 #     conn.close()
--- django-wiki/bsi/models.py
+++ django-wiki/bsi/models.py
@@ -1,16 +1,2 @@
 from django.db import models
-from wiki.models import Article, ArticleRevision
 
-
-class BSIArticle(Article):
-    """
-
-    """
-    bsi_id = models.CharField(null=False, blank=False, max_length=32)
-
-
-class BSIArticleRevision(ArticleRevision):
-    """
-
-    """
-    location = models.TextField(null=False, blank=False)
