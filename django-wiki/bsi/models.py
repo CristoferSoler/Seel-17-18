c71c100c06fb5226b616855bade33ec1c219d389
EC2 Default User <ec2-user@ip-172-31-36-255.eu-central-1.compute.internal>
2017-11-26 12:47:46 +0000

    extended the model to include BSI attributes

--- django-wiki/bsi/models.py
+++ django-wiki/bsi/models.py
@@ -1,2 +1,22 @@
 from django.db import models
+from wiki.models.article import Article
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+class ArticleAttr(models.Model):
+    article = models.OneToOneField(Article, on_delete=models.CASCADE)
+    isArchive = models.BooleanField(default=False)
+    reference_id = models.ForeignKey(Article, blank=True, related_name='is_linked_to')
+    bsi_id = models.CharField(max_length=10, blank=True)
+
+@receiver(post_save, sender=Article)
+def create_article_attr(sender, instance, created, **kwargs):
+    if created:
+        ArticleAttr.objects.create(article=instance)
+
+@receiver(post_save, sender=Article)
+def save_article_attr(sender, instance, **kwargs):
+    instance.articleattr.save()
+
 
