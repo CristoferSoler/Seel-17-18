54b98202f0a10615110fe93fd21ad8db51997aad
joudsi <joudsayedissa@gmail.com>
2017-11-26 11:35:46 +0100

    extending the models and views of the django-wiki

--- wiki/bsi/views.py
+++ wiki/bsi/views.py
@@ -1,12 +1,20 @@
+from msilib.schema import ListView
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 #from .models import <tableName>
+# from bsi.core.paginator import WikiPaginator
+from wiki.models.article import Article
+from wiki.views.article import SearchView
+
 
 def index(request):
-   # <var:all_tuples_of_a_Table:Name> = <tableName>.objects.all()
+    all_articles = Article.objects.all()
+
     template = loader.get_template('bsi/index.html')
     context = {
-    #     '<all_tuples_of_a_Table:Name>':<var:all_tuples_of_a_Table:Name>,
+        'all_articles':all_articles,
     }
     return HttpResponse(template.render(context, request))
+
