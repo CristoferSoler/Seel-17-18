27dbf747a5a1c8854d9a6f2c86fb1ac191525016
ferhat_s <ferhat.saritas@developers.luckycloud.de>
2017-11-30 13:09:24 +0100

    register: still buggy

--- django-wiki/bsi/views.py
+++ django-wiki/bsi/views.py
@@ -1,11 +1,15 @@
-
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 #from .models import <tableName>
 # from bsi.core.paginator import WikiPaginator
 from wiki.models.article import Article
 from wiki.views.article import SearchView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django import forms
 
 
 def index(request):
@@ -25,3 +29,49 @@ def login(request):
         'all_articles':all_articles,
     }
     return HttpResponse(template.render(context, request))
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(
+        required=True,
+        label='Username',
+        max_length=32
+    )
+    email = forms.CharField(
+        required=True,
+        label='Email',
+        max_length=32,
+    )
+    password = forms.CharField(
+        required=True,
+        label='Password',
+        max_length=32,
+        widget=forms.PasswordInput()
+    )
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            userObj = form.cleaned_data
+            username = userObj['username']
+            email =  userObj['email']
+            password =  userObj['password']
+            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+                User.objects.create_user(username, email, password)
+                user = authenticate(username = username, password = password)
+                login(user)
+                return HttpResponseRedirect('/')
+            else:
+                raise forms.ValidationError('Looks like a username with that email or password already exists')
+
+    else:
+        #form = UserRegistrationForm()
+
+        return HttpResponseRedirect('/')
