ad5c96919ea36bfe3ed1ce53488095a0e40723d4
kubra <kubraaseyhan@gmail.com>
2017-11-30 16:58:43 +0300

    bug fix

--- django-wiki/bsi/urls.py
+++ django-wiki/bsi/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^signin/', views.signin, name='singin'),
+    url(r'^signin/', views.signin, name='login'),
     url(r'^signup/', views.signup, name='signup')
 ] 
