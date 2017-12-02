b2b4ff9d022b50350fb946d12a7510f053de87f5
SMILElikeJONNY <jonathan-zimmermann94@gmx.de>
2017-12-02 14:03:54 +0100

    fix merge

--- django-wiki/bsi/urls.py
+++ django-wiki/bsi/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from . import ugaViews
 from django.contrib.auth import views as auth_views
 from wiki.views import article
 
@@ -11,4 +12,7 @@ urlpatterns = [
     url(r'^(?P<path>.+/|)_create/', article_create_view_class.as_view(), {'template_name': 'bsi/create_article.html'},  name='create'),
     url(r'^login/', auth_views.login, {'template_name': 'bsi/account/login.html'}, name='login'),
     url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
-    url(r'^register/', views.register, name='register'),]
+    url(r'^register/', views.register, name='register'),
+    url(r'^bsicatalog/', views.bsicatalog, name='bsicatalog'),
+    url(r'ugarticles/',ugaViews.overviewUGA, name='ugarticles')
+] 
