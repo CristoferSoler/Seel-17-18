b635d532b7ec761fe1fd0aba1d36cb024db034a0
pkabus <peter.kabus@gmx.de>
2017-12-02 16:48:53 +0100

    Created archive transaction model with test case

--- django-wiki/archive/models.py
+++ django-wiki/archive/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from wiki.models import Article, URLPath
+from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from wiki.decorators import get_article
+from wiki.models.urlpath import URLPath, transaction
+from wiki.models.article import Article
+from wiki.views.mixins import ArticleMixin
 
-
-# Create your models here.
 
 # class ArchivedArticle(models.Model):
 #     article = models.OneToOneField('Article', verbose_name=_('archived article'),
@@ -16,19 +19,30 @@ from wiki.models import Article, URLPath
 #                                        'Archive which contains this archived article.'), on_delete=models.CASCADE, )
 #
 
-class Archive(Article):
-    def archiveArticle(self, article):
-        """
 
-        """
+class ArchiveTransaction(models.Model):
+    archive_root = models.OneToOneField('wiki.URLPath', verbose_name='archive parent article',
+                                        blank=False, null=False, related_name='archive_root_article',
+                                        on_delete=models.CASCADE, )
 
-        article.urlpath.parent = self.urlpath
-        # article.urlpath.slug = form.cleaned_data['slug']
-        article.urlpath.save()
+    @classmethod
+    def create(cls, urlpath):
+        return cls(archive_root=urlpath)
 
-        # Reload url path from database
-        article.urlpath = models.URLPath.objects.get(pk=article.urlpath.pk)
+    # @transaction.atomic
+    def archive(self, urlpath):
+
+        # urlpath.slug = new_slug
+        urlpath.parent = self.archive_root
+        urlpath.save()
+
+        # self.urlpath.parent = parent
+        # self.urlpath.slug = slug
+        # self.urlpath.save()
+
+        # Reload url path form database
+        urlpath = URLPath.objects.get(pk=urlpath.pk)
 
         # Use a copy of ourself (to avoid cache) and update article links again
-        for ancestor in models.Article.objects.get(pk=article.pk).ancestor_objects():
+        for ancestor in Article.objects.get(pk=urlpath.article.pk).ancestor_objects():
             ancestor.article.clear_cache()
--- django-wiki/archive/tests.py
+++ django-wiki/archive/tests.py
@@ -1,51 +1,60 @@
+from django.db.models.base import ModelBase
 from django.test import TestCase
 from wiki.models import Article, ArticleRevision, URLPath, Site
-from archive.models import Archive
+from archive.models import ArchiveTransaction
 
 
 # Create your tests here.
 class ArchiveTestCase(TestCase):
     def setUp(self):
-        ArchiveTestCase.createRoot(rev_title="root")
-        ArchiveTestCase.createArticle(rev_title="subarticle1", slug="subarticle1")
+        ArchiveTestCase.createArticle(rev_title="root", article_type=Article, parent=None, slug=None)
+        ArchiveTestCase.createArticle(rev_title="subarticle1", article_type=Article, parent=URLPath.objects.filter(slug=None)[0], slug="subarticle1")
+        ArchiveTestCase.createArticle(rev_title="archive", article_type=Article, parent=URLPath.objects.filter(slug=None)[0], slug="archive")
         # todo create archive here <- implement create function in models or views
 
     def test_article_moved_to_archive(self):
-        articleId = URLPath.objects.filter(slug='subarticle1').values_list('article')[0]
-        article = Article.objects.get(pk=articleId[0])
-        parentId = URLPath.objects.filter(slug='subarticle1').values_list('parent')[0]
-        parent = Article.objects.get(pk=parentId[0])
-        rootId = URLPath.objects.filter(slug=None).values_list('article')[0]
-        root = Article.objects.get(pk=rootId[0])
+
+        # get test sub article
+        # article_id = URLPath.objects.filter(slug='subarticle1').values_list('article')[0]
+        article = URLPath.objects.get(slug='subarticle1').article
+        # article = Article.objects.get(pk=article_id)
+
+        # get parent of test article
+        parent = URLPath.objects.get(slug='subarticle1').parent.article
+        # parent = Article.objects.get(pk=parent_id[0])
+
+        # get root
+        root = URLPath.objects.get(slug=None).article
+        # root = Article.objects.get(pk=root_id[0])
+
+        # parent of sub article should be root
         self.assertTrue(parent == root, "article 'parent' should be the root")
 
-    @classmethod
-    def createRoot(cls, rev_title):
-        revision_kwargs = {'content': 'content', 'user_message': 'Crawler input', 'ip_address': 'None'}
-        site = Site.objects.get_current()
+        # get archive
+        archive_path = URLPath.objects.get(slug='archive')
+        archive = archive_path.article
 
-        article_kwargs = {}
-        article = Article(**article_kwargs)
-        article.add_revision(ArticleRevision(title=rev_title, **revision_kwargs),
-                             save=True)
-        article.save()
-        newpath = URLPath.objects.create(
-            site=site,
-            parent=None,
-            slug=None,
-            article=article)
+        # archive article
+        transaction = ArchiveTransaction.create(archive_path)
+        transaction.archive(URLPath.objects.get(slug='subarticle1'))
 
-        article.add_object_relation(newpath)
+        parent = URLPath.objects.get(slug='subarticle1').parent.article
+
+        # parent of sub article should be archive
+        self.assertTrue(parent == archive, "article 'archive' should be the parent")
+
+
 
     @classmethod
-    def createArticle(cls, rev_title, slug):
+    def createArticle(cls, article_type, rev_title, parent, slug):
         revision_kwargs = {'content': 'content', 'user_message': 'Crawler input', 'ip_address': 'None'}
-        parent = URLPath.objects.filter(slug=None)[0]
 
         site = Site.objects.get_current()
 
         article_kwargs = {}
-        article = Article(**article_kwargs)
+        # article_cls = ModelBase(article_type, (), {})
+        article = article_type(**article_kwargs)
+        # test = Article(**article_kwargs)
         article.add_revision(ArticleRevision(title=rev_title, **revision_kwargs),
                              save=True)
         article.save()
