e1048da49bbda01567bf5f940df8420aaf03931e
SMILElikeJONNY <jonathan-zimmermann94@gmx.de>
2017-12-02 13:56:17 +0100

    add a overview page for uga articles. Of course they is very ugly :D

---
+++ django-wiki/bsi/ugaViews.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def createAricle(name,user,date):
+    return {'name':name, 'user':user,'date':date}
+
+def overviewUGA(request):
+    articles = [createAricle('Test1','user1','20.1'),createAricle('Test2','user2','20.1'),createAricle('Test3','user3','20.1'),createAricle('Test4','user4','20.1'),createAricle('Test5','user5','20.1'),createAricle('Test6','user6','20.1'),createAricle('Test7','user7','20.1'),createAricle('Test8','user8','20.1'),createAricle('Test9','user9','20.1'),createAricle('Test10','user10','20.1'),createAricle('Test11','user11','20.1'),createAricle('Test1','user12','20.12'),createAricle('Test13','user13','20.1'),createAricle('Test14','user14','20.1'),createAricle('Test15','user15','20.1'),createAricle('Test16','user16','20.1'),createAricle('Test17','user17','20.1'),createAricle('Test18','user18','20.1'),createAricle('Test19','user19','20.1'),createAricle('Test20','user20','20.1'),createAricle('Test21','user21','20.1')]
+    page = request.GET.get('page', 1)
+    paginator = Paginator(articles, 10)
+    print(paginator.count)
+    try:
+        articles1 = paginator.page(page)
+    except PageNotAnInteger:
+        articles1 = paginator.page(1)
+    except EmptyPage:
+        articles1 = paginator.page(paginator.num_pages)
+
+
+    return render(request,'uga/overviewUGA.html',{'articles':articles1})
