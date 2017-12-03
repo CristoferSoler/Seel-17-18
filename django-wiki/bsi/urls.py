b1481fb7dd08c30c3b2a3435bfcf2c057fcee38d
Andisa Dewi <andisa.dewi@icloud.com>
2017-12-03 17:03:52 +0100

    extended SearchView class

--- django-wiki/bsi/urls.py
+++ django-wiki/bsi/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 from wiki.views import article
 from . import ugaViews
+from .views import BSISearchView
 
 article_create_view_class = article.Create
 
@@ -14,6 +15,7 @@ urlpatterns = [
     url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^register/', views.register, name='register'),
     url(r'^bsicatalog/', views.bsicatalog, name='bsicatalog'),
+    url(r'^search/', BSISearchView.as_view(), name='bsisearch'),
     url(r'ugarticles/',ugaViews.overviewUGA, name='ugarticles'),
     url(r'changePassword/', auth_views.password_change, {'template_name': 'bsi/account/accountsSettings.html'}, name='password_change'),
     url(r'^accounts/password/change/done/$', auth_views.password_change_done, {'template_name': 'bsi/account/passwordChangeDone.html'}, name='password_change_done'),
