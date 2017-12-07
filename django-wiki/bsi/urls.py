da64389072a6b6ffc7fb397a53420e35382e96f9
SMILElikeJONNY <jonathan-zimmermann94@gmx.de>
2017-12-07 16:55:10 +0100

    little step closer to show a create article template

--- django-wiki/bsi/urls.py
+++ django-wiki/bsi/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 from wiki.models import URLPath
 
-from . import views
-from . import ugaViews
+from . import views, ugaViews
 from django.contrib.auth import views as auth_views
 from wiki.views import article
 from .views import BSISearchView, BSIArticleView, UGACreate, CreateRoot
@@ -12,7 +11,6 @@ article_create_view_class = article.Create
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^bsicatalog/', views.bsicatalog, name='bsicatalog'),
-    url(r'^(?P<path>.+/|)_create/', article_create_view_class.as_view(), {'template_name': 'bsi/create_article.html'},  name='create'),
     url(r'^login/', auth_views.login, {'template_name': 'bsi/account/login.html'}, name='login'),
     url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^register/', views.register, name='register'),
@@ -24,4 +22,5 @@ urlpatterns = [
     url(r'ugarticles/',ugaViews.overviewUGA, name='ugarticles'),
     url(r'changePassword/', auth_views.password_change, {'template_name': 'bsi/account/accountsSettings.html'}, name='password_change'),
     url(r'^accounts/password/change/done/$', auth_views.password_change_done, {'template_name': 'bsi/account/passwordChangeDone.html'}, name='password_change_done'),
+    url(r'^(?P<path>.+/|)_create/', views.UGACreate.as_view(), name='create'),
    ]
