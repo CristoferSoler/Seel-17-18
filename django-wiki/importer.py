2056c18ee8451188e1a17fc6124b104a6199f5b3
TLara92 <lara.tunbari92@gmail.com>
2017-12-07 07:28:11 +0100

    Importing BSI articles to database (insert BSI articles to db)- Not finished yet!

--- django-wiki/importer.py
+++ django-wiki/importer.py
@@ -10,11 +10,20 @@ django.setup()
 
 from wiki.models import Article, URLPath, Site, ArticleRevision
 from bsiwiki import settings
+from bsi.models import BSI, UGA
 from wiki.core.markdown import article_markdown
 
 
+def checkType(BSIArticleid):
+    if "B" in BSIArticleid:
+        return "components"
+    if "G" in BSIArticleid:
+        return "threats"
+    if "M" in BSIArticleid:
+        return "countermeasures"
 # from website import model
 def insert_BSI_db():
+    site = Site.objects.get_current()
     for dirpath, dirnames, filenames in os.walk(settings.CRAWLER_DIRECTORY):
         for filename in [f for f in filenames if f.endswith(".md")]:
             # get the drive and the filepath
@@ -27,20 +36,14 @@ def insert_BSI_db():
             # read md file content in variable
             with open(path_and_file) as data_file:
                 content = data_file.readlines()
-                #print(content)
-            #revision_kwargs = {'content': content, 'user_message': 'Crawler input', 'ip_address': 'None'}
-            #parent = URLPath.objects.filter(slug=None)[0]
-            #slug = id
-
-            #site = Site.objects.get_current()
-            #create_article_with_revision(site=site, parent=parent, slug=id, content=content, rev_title=file_name, rev_kwargs=revision_kwargs)
-
+                revision_kwargs = {'content': content, 'user_message': 'Crawler input', 'ip_address': '0.0.0.0'}
+                parent = URLPath.objects.filter(slug=checkType(id))[0] #example.com/root/bsi/components or threats or counterMeasures
+                create_article_with_revision(site=site, parent=parent, slug= id, content=content, rev_title=file_name, rev_kwargs=revision_kwargs)
 
 def create_article_with_revision(site, parent, slug, content, rev_title, rev_kwargs):
     article_kwargs = {}
     article = Article(**article_kwargs)
-    article.add_revision(ArticleRevision(title=rev_title, **rev_kwargs),
-                         save=True)
+    article.add_revision(ArticleRevision(title=rev_title, **rev_kwargs), save=True)
     article.save()
     newpath = URLPath.objects.create(
         site=site,
@@ -48,16 +51,18 @@ def create_article_with_revision(site, parent, slug, content, rev_title, rev_kwa
         slug=slug,
         article=article)
     article.add_object_relation(newpath)
+    bsiArt = BSI(article=article, bsi_id=slug)
+    bsiArt.save()
     if not slug:
         slug = '/'
     print('Created article at ', slug)
 
 if __name__ == '__main__':
 
-    # Article.objects.all().delete() # thats horribly slow
+     #Article.objects.all().delete() # thats horribly slow
     #root_kwargs = {'content': '', 'user_message': 'Crawler input', 'ip_address': 'None'}
     #create_article_with_revision(site = Site.objects.get_current(),parent=None, slug=None, content='', rev_title='Root', rev_kwargs=root_kwargs)
     insert_BSI_db()
-
+    #print(checkType("G 2.34565"))
     #finally:
 #     conn.close()
