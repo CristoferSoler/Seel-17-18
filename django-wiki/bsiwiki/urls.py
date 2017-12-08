7441cfb0fababe8063d20fa49ef583ebf848131a
EC2 Default User <ec2-user@ip-172-31-36-255.eu-central-1.compute.internal>
2017-12-08 12:04:32 +0000

    added decorators, modified dir.html, fixed urls

--- django-wiki/bsiwiki/urls.py
+++ django-wiki/bsiwiki/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from wiki.urls import get_pattern as get_wiki_pattern
 from django_nyt.urls import get_pattern as get_nyt_pattern
 from archive import views
+from wiki.views.article import Dir, ArticleView
 
 # class BSIWikiUrlPattern(object):
-# archive_root_view_class = views.ArchiveRoot
+#archive_root_view_class = views.ArchiveRoot
 archive_dir_view_class = views.ArchiveDir
 
 # def get_urls(self):
@@ -37,8 +38,8 @@ urlpatterns = [
     # todo if the url archive/ is called, the wiki should list (_dir) all sub-archives
     # url(r'^archive/$', archive_dir_view_class.as_view(),
     #     name='archive_root'),
-    url(r'^archive/(?P<path>.+/|)$', archive_dir_view_class.as_view(),
-        name='archive_dir'),
+    
+    url(r'^archive/', include('archive.urls')),
     url(r'', get_wiki_pattern()),
 ]
 # return urlpatterns
