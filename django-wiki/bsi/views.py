f28dd13b080c361c709d88eb5758f4386f8eefbd
SMILElikeJONNY <jonathan-zimmermann94@gmx.de>
2017-12-09 14:21:52 +0100

    fix merge

--- django-wiki/bsi/views.py
+++ django-wiki/bsi/views.py
@@ -48,4 +48,80 @@ class BSIArticleView(ArticleView):
             *args,
             **kwargs)
 
-# Create your views here.
+    def get_context_data(self, **kwargs):
+        kwargs['selected_tab'] = 'view'
+        return ArticleMixin.get_context_data(self, **kwargs)
+
+
+class BSISearchView(SearchView):
+    template_name = "bsi/search.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        return super(BSISearchView, self).dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return super(BSISearchView, self).get_queryset()
+
+    def get_context_data(self, **kwargs):
+        return super(BSISearchView, self).get_context_data(**kwargs)
+
+
+def index(request):
+    all_articles = Article.objects.all()
+
+    template = loader.get_template('bsi/index.html')
+    context = {
+        'all_articles':all_articles,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def bsicatalog(request):
+    all_articles = Article.objects.all()
+
+    template = loader.get_template('bsi/article.html')
+    context = {
+        'all_articles':all_articles,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(
+        required=True,
+        label='Username',
+        max_length=32
+    )
+    email = forms.CharField(
+        required=True,
+        label='Email',
+        max_length=32,
+    )
+    password = forms.CharField(
+        required=True,
+        label='Password',
+        max_length=32,
+        widget=forms.PasswordInput()
+    )
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'bsi/account/register.html', {'form': form})
+
+def create(request):
+    return render(request, 'bsi/create_article.html')
