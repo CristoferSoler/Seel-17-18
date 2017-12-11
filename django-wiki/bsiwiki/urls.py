d4ca6491e604b3602b4b12ff899071483d6c6646
pkabus <peter.kabus@gmx.de>
2017-12-11 16:37:01 +0100

    moved login/registration urls out of the bsi app into the main urls.py

--- django-wiki/bsiwiki/urls.py
+++ django-wiki/bsiwiki/urls.py
@@ -17,10 +17,20 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from wiki.urls import get_pattern as get_wiki_pattern
 from django_nyt.urls import get_pattern as get_nyt_pattern
+from django.contrib.auth import views as auth_views
+from bsi import views
 
 urlpatterns = [
+    url(r'^login/', auth_views.login, {'template_name': 'bsi/account/login.html'}, name='login'),
+    url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^register/', views.register, name='register'),
+    url(r'changePassword/', auth_views.password_change, {'template_name': 'bsi/account/accountsSettings.html'},
+        name='password_change'),
+    url(r'^accounts/password/change/done/$', auth_views.password_change_done,
+        {'template_name': 'bsi/account/passwordChangeDone.html'}, name='password_change_done'),
     url(r'^admin/', admin.site.urls),
     url(r'^notifications/', get_nyt_pattern()),
+    url(r'^archive/', include('archive.urls')),
     url(r'^wiki/', get_wiki_pattern()),
-    url(r'^', include('bsi.urls'))
+    url(r'^', include('bsi.urls')),
 ]
