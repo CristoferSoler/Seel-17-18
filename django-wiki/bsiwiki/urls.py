dfeb9037b2a1a24e664a4049d649398b25c03834
EC2 Default User <ec2-user@ip-172-31-36-255.eu-central-1.compute.internal>
2017-12-11 19:18:16 +0000

    cleanup, fixed the template

--- django-wiki/bsiwiki/urls.py
+++ django-wiki/bsiwiki/urls.py
@@ -17,35 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from wiki.urls import get_pattern as get_wiki_pattern
 from django_nyt.urls import get_pattern as get_nyt_pattern
-from archive import views
-from wiki.views.article import Dir, ArticleView
 
-# class BSIWikiUrlPattern(object):
-#archive_root_view_class = views.ArchiveRoot
-archive_dir_view_class = views.ArchiveDir
-
-# def get_urls(self):
-#     urlpatterns = self.get_root_urls()
-# urlpatterns += self.get_article_path_urls()
-# return urlpatterns
-
-
-# @staticmethod
-# def get_root_urls():
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^notifications/', get_nyt_pattern()),
-    # todo if the url archive/ is called, the wiki should list (_dir) all sub-archives
-    # url(r'^archive/$', archive_dir_view_class.as_view(),
-    #     name='archive_root'),
-    
     url(r'^archive/', include('archive.urls')),
     url(r'', get_wiki_pattern()),
 ]
-# return urlpatterns
 
-
-# def get_article_path_urls(self):
-#     urlpatterns = [
-#     ]
-#     return urlpatterns
