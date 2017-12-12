355bf9ab9266b953cc9acce149b210f3600d6d76
pkabus <peter.kabus@gmx.de>
2017-12-12 15:16:48 +0100

    create functions for UGA and BSI, also backend functions to add and remove links to from uga -> bsi. Also, testcase

--- django-wiki/bsi/models.py
+++ django-wiki/bsi/models.py
@@ -1,24 +1,49 @@
 from django.db import models
-#from wiki.models.article import Article
+# from wiki.models.article import Article
 from wiki.models import Article, URLPath, Site, ArticleRevision
 
 
-# Create your models here.
 class UGA(models.Model):
-    article = models.OneToOneField(Article, on_delete=models.CASCADE, primary_key=True)
+    urlpath = models.OneToOneField(URLPath, on_delete=models.CASCADE, primary_key=True)
+
+    @classmethod
+    def create(cls, parent, slug, title, **rev_kwargs):
+        url = URLPath.create_urlpath(parent=parent, slug=slug, title=title, **rev_kwargs)
+        if not url.path.startswith('uga') or len(url.path) == 3:
+            raise ValueError("A user article is supposed to be a child of 'uga' and it cannot be 'uga' itself.")
+        uga = cls(urlpath=url)
+        uga.save()
+
+    def add_link_to_bsi(self, bsi):
+        bsi.references.add(self)
+
+    def remove_link_from_bsi(self, bsi):
+        bsi.references.remove(self)
+
+    def __str__(self):
+        return 'UGA with path: ' + self.urlpath.__str__()
 
 
 class BSI(models.Model):
-   # article = models.OneToOneField(Article, on_delete=models.CASCADE, primary_key=True)
-    #bsi_id = models.CharField(max_length=10, blank=True)
-    url = models.OneToOneField('wiki.URLPath')
+    urlpath = models.OneToOneField('wiki.URLPath', on_delete=models.CASCADE, )
     references = models.ManyToManyField(UGA, blank=True, related_name='is_linked_to')
 
-    def isThreat(self):
-        return self.url.slug.startswith('G')
+    @classmethod
+    def create(cls, parent, slug, title, **rev_kwargs):
+        url = URLPath.create_urlpath(parent=parent, slug=slug, title=title, **rev_kwargs)
+        if not url.path.startswith('bsi') or len(url.path) == 3:
+            raise ValueError("A bsi article is supposed to be a child of 'bsi' and it cannot be 'bsi' itself.")
+        bsi = cls(urlpath=url)
+        bsi.save()
 
-    def isComponent(self):
-        return self.url.slug.startswith('B')
+    def __str__(self):
+        return 'BSI article with path: ' + self.urlpath.__str__()
 
-    def isCountermeasure(self):
-        return self.url.slug.startswith('M')
+    # def isThreat(self):
+    #     return self.url.slug.startswith('G')
+    #
+    # def isComponent(self):
+    #     return self.url.slug.startswith('B')
+    #
+    # def isCountermeasure(self):
+    #     return self.url.slug.startswith('M')
