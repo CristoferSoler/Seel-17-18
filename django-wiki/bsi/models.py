754f881cca6096d06464f489e2effd36d4928bcd
TLara92 <lara.tunbari92@gmail.com>
2017-12-14 20:33:04 +0100

    merge branch "importer"

--- django-wiki/bsi/models.py
+++ django-wiki/bsi/models.py
@@ -1,8 +1,17 @@
-from django.db import models
-# from wiki.models.article import Article
-from wiki.models import Article, URLPath, Site, ArticleRevision
+from django.db import models , IntegrityError
+#from wiki.models.article import Article
+from wiki.models import Article, URLPath, Site, ArticleRevision, transaction
+from enumfields import EnumField
+from enumfields import Enum
+from django.http import Http404
 
 
+class BSI_Article_type(Enum):
+    COMPONENT = 'C'
+    THREAT = 'G'
+    IMPLEMENTATIONNOTES = 'N'
+
+# Create your models here.
 class UGA(models.Model):
     urlpath = models.OneToOneField(URLPath, on_delete=models.CASCADE, primary_key=True)
 
@@ -23,27 +32,62 @@ class UGA(models.Model):
     def __str__(self):
         return 'UGA with path: ' + self.urlpath.__str__()
 
-
 class BSI(models.Model):
-    urlpath = models.OneToOneField('wiki.URLPath', on_delete=models.CASCADE, )
+    url = models.OneToOneField(URLPath, on_delete=models.CASCADE, primary_key=True)
     references = models.ManyToManyField(UGA, blank=True, related_name='is_linked_to')
+    articleType = EnumField(BSI_Article_type,blank=True, max_length=1, null = True)
 
     @classmethod
-    def create(cls, parent, slug, title, **rev_kwargs):
+    def get_or_create_bsi_root(cls, content):
+        try:
+           bsiRoot = URLPath.objects.get(slug='bsi')
+
+        except URLPath.DoesNotExist:
+            root = URLPath.objects.filter(slug=None)
+            if (not root):
+                site = Site.objects.get_current()
+                kwargs = {'content': "", 'user_message': 'BSI.create', 'ip_address': '0.0.0.0'}
+                root = URLPath.create_root(site=site, title="Root", request=None, **kwargs)
+            else:
+                root = root[0]
+            rev_kwargs = {'content': content, 'user_message': 'BSI.create', 'ip_address': '0.0.0.0'}
+            bsiRoot = URLPath.create_urlpath(parent=root, slug='bsi', title='BSI',
+                                                **rev_kwargs)
+
+        return bsiRoot
+
+    @classmethod
+    def get_or_create_bsi_subroots(cls, slug, user_msg, content, title):
+        bsi_root = BSI.get_or_create_bsi_root('')
+        subroot = URLPath.objects.filter(slug=slug)
+        if (not subroot):
+            rev_kwargs = {'content': content, 'user_message': user_msg, 'ip_address': '0.0.0.0'}
+            subroot = URLPath.create_urlpath(parent=bsi_root, slug=slug, title=title,
+                                        **rev_kwargs)
+        else:
+            subroot = subroot[0]
+        return subroot
+
+    @classmethod
+    @transaction.atomic
+    def create(cls, parent, slug, title, article_type, **rev_kwargs):
         url = URLPath.create_urlpath(parent=parent, slug=slug, title=title, **rev_kwargs)
         if not url.path.startswith('bsi') or len(url.path) == 3:
             raise ValueError("A bsi article is supposed to be a child of 'bsi' and it cannot be 'bsi' itself.")
-        bsi = cls(urlpath=url)
+        bsi = cls(url=url, articleType=article_type)
         bsi.save()
 
-    def __str__(self):
-        return 'BSI article with path: ' + self.urlpath.__str__()
+    @classmethod
+    def get_articles_by_type(cls, article_type):
+         article_urlpaths = []
+         articles = BSI.objects.filter(articleType= article_type)
+         if not articles:
+             raise Http404("No articles found that matches the specified search type: ", article_type)
+         for article in articles:
+             article_urlpaths.append(article)
+         return article_urlpaths
 
-    # def isThreat(self):
-    #     return self.url.slug.startswith('G')
-    #
-    # def isComponent(self):
-    #     return self.url.slug.startswith('B')
-    #
-    # def isCountermeasure(self):
-    #     return self.url.slug.startswith('M')
+
+    def __str__(self):
+        return 'BSI article with path: ' + self.url.__str__()
+
