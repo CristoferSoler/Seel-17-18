e1e17a06d3c94afa94a9eec62fbafece03d14918
EC2 Default User <ec2-user@ip-172-31-36-255.eu-central-1.compute.internal>
2017-12-14 13:07:35 +0000

    fixed archive templates

--- django-wiki/archive/decorators.py
+++ django-wiki/archive/decorators.py
@@ -1,51 +1,27 @@
-import json
-
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseNotFound, \
-    HttpResponseForbidden, HttpResponseRedirect, Http404
-from django.shortcuts import redirect, get_object_or_404
-from django.template.context import RequestContext
-from django.template.loader import render_to_string
-from django.utils.http import urlquote
-
-from functools import wraps
-
-from six.moves import filter
-
-from wiki.core.exceptions import NoRootURL
-from wiki.conf import settings
-import pdb
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import redirect
+from .models import Archive
 
 def get_archive_article(func=None, can_read=True, deleted_contents=False):
 
     def wrapper(request, *args, **kwargs):
         from . import models
-        #pdb.set_trace()
         path = kwargs.pop('path', None)
         article_id = kwargs.pop('article_id', None)
         
-        # append archive to the beginning of path
-        path = "archive/" + path.lstrip("/")
+        path = path.rstrip("/")
         urlpath = None
         
         # fetch by urlpath.path
         if not path is None:
             try:
-                urlpath = models.URLPath.get_by_path(path, select_related=True)
-            except NoRootURL:
-                return redirect('wiki:root_create')
+                if(path == ''):
+                    urlpath = Archive.get_or_create_archive_root()
+                else:
+                    urlpath = Archive.get_archive_by_slug(path).archive_url
             except models.URLPath.DoesNotExist:
                 raise Http404("No archive found that matches the specified path: ", path)
-                '''
-                try:
-                    pathlist = list(filter(lambda x: x!="", path.split("/"),))
-                    path = "/".join(pathlist[:-1])
-                    parent = models.URLPath.get_by_path(path)
-                    return HttpResponseRedirect(reverse("wiki:create", kwargs={'path': parent.path,}) + "?slug=%s" % pathlist[-1])
-                except models.URLPath.DoesNotExist:
-                    c = RequestContext(request, {'error_type' : 'ancestors_missing'})
-                    return HttpResponseNotFound(render_to_string("wiki/error.html", context_instance=c))
-                '''
             if urlpath.article:
                 # urlpath is already smart about prefetching items on article (like current_revision), so we don't have to
                 article = urlpath.article
@@ -55,53 +31,11 @@ def get_archive_article(func=None, can_read=True, deleted_contents=False):
                 urlpath.delete()
                 return HttpResponseRedirect(return_url)
         
-        '''
-        # fetch by article.id
-        elif article_id:
-            #TODO We should try to grab the article form URLPath so the caching is good, and fall back to grabbing it from Article.objects if not
-            articles = models.Article.objects
-            
-            article = get_object_or_404(articles, id=article_id)
-            try:
-                urlpath = models.URLPath.objects.get(articles__article=article)
-            except models.URLPath.DoesNotExist as noarticle:
-                models.URLPath.MultipleObjectsReturned = noarticle
-                urlpath = None
-        
-        
-        else:
-            raise TypeError('You should specify either article_id or path')
-        '''
-
         if not deleted_contents:    
             # If the article has been deleted, show a special page.        
             if urlpath:
                 if urlpath.is_deleted(): # This also checks all ancestors
                     return redirect('wiki:deleted', path=urlpath.path)
-            '''
-            else:
-                if article.current_revision and article.current_revision.deleted:
-                    return redirect('wiki:deleted', article_id=article.id)
-            
-        
-        if article.current_revision.locked and not_locked:
-            return response_forbidden(request, article, urlpath)
-
-        if can_read and not article.can_read(request.user):
-            return response_forbidden(request, article, urlpath)
-        
-        if (can_write or can_create) and not article.can_write(request.user):
-            return response_forbidden(request, article, urlpath)
-
-        if can_create and not (request.user.is_authenticated() or settings.ANONYMOUS_CREATE):
-            return response_forbidden(request, article, urlpath)
-        
-        if can_delete and not article.can_delete(request.user):
-            return response_forbidden(request, article, urlpath)
-            
-        if can_moderate and not article.can_moderate(request.user):
-            return response_forbidden(request, article, urlpath)
-        '''
 
         kwargs['urlpath'] = urlpath
         
--- django-wiki/archive/views.py
+++ django-wiki/archive/views.py
@@ -1,29 +1,15 @@
-from django.http import Http404
-from django.shortcuts import render
 from django.utils.decorators import method_decorator
-from django.views.generic.list import ListView
-from django.db.models import Q
 from wiki import forms
-# from wiki.core.paginator import WikiPaginator
-from wiki.decorators import get_article
-from wiki.models.urlpath import URLPath
 from wiki.views.article import Dir, ArticleView
-from wiki.views.mixins import ArticleMixin
-from .models import Archive
+from .models import Archive, ArchiveTransaction
 from .decorators import get_archive_article
-import pdb
-
 
 class ArchiveDir(Dir):
-    template_name = "archive/dir.html"
+    template_name = "archive/archivelist.html"    
 
     @method_decorator(get_archive_article(can_read=True))
     def dispatch(self, request, article, *args, **kwargs):
-        # find archive by urlpath slug, if it is not present, 404 will occur
-        path = kwargs.get('urlpath')
-        if (path != Archive.get_archive_root()):
-            Archive.get_archive_by_slug(kwargs.get('urlpath').slug)
-
+       
         # temp fix 
         self.filter_form = forms.DirFilterForm(request.GET)
         if self.filter_form.is_valid():
@@ -39,13 +25,15 @@ class ArchiveDir(Dir):
     def get_context_data(self, **kwargs):
         return super(ArchiveDir, self).get_context_data(**kwargs)
 
-
 class ArchiveArticleView(ArticleView):
 
+    # TODO
+    # template_name = "archive/archive_article.html"
+    
     def dispatch(self, request, article, *args, **kwargs):
-        # check ArchiveTransactionHere
-        # pdb.set_trace()
-
+        # check ArchiveTransaction here
+        kwargs['path'] = ArchiveTransaction.get_path_by_slug(kwargs.get('path'), article)
+        article = kwargs.get('path').article
         return super(ArticleView, self).dispatch(request, article, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
