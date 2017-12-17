77bbb83dc65a70ace54d79b3b1eecdafea026ba4
SMILElikeJONNY <jonathan-zimmermann94@gmx.de>
2017-12-17 19:27:38 +0100

    added to the wizard dialog the most frequence topic to the question

--- django-wiki/bsi/static/bsi/js/wizard/wizard.js
+++ django-wiki/bsi/static/bsi/js/wizard/wizard.js
@@ -5,7 +5,10 @@ function initWizard(components,sortedTopics) {
     var components = JSON.parse(components)['components'];
     //speichern der Ausgangsdaten
     orginalTopic = components;
-    window.alert(JSON.parse(sortedTopics)[1])
+
+    var oldText = 'Are there any problems with ';
+    $("#questionText").text(oldText + JSON.parse(sortedTopics)[1]);
+    //window.alert(JSON.parse(sortedTopics)[1])
     components.forEach(function (element) {
         //window.alert(JSON.stringify(element))
     })
