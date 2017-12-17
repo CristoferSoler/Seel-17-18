8d5a375e5930fd213698c98305d46ed61678ef1d
TLara92 <lara.tunbari92@gmail.com>
2017-12-17 17:44:38 +0100

    Get the cross reference relation and append it to the specific articles (components) before import the articles content to the database

--- django-wiki/Scripts/bsiImporter.py
+++ django-wiki/Scripts/bsiImporter.py
@@ -4,6 +4,9 @@ import configparser
 import win32com.client
 import os
 import sys
+import string
+import filecmp
+
 
 sys.path.append("C:/githubRepo/Seel-17-18/django-wiki")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bsiwiki.settings")
@@ -18,6 +21,7 @@ from bsi.models import BSI, UGA,BSI_Article_type
 txtFolderDir = './Cross_Reference_Files/'
 excelFolderDir = './Cross_Reference_Tables/'
 macroFileDir = './Cross Reference 2018.xlsm/'
+CRF = './CRF/'
 system_devices = ["APP", "SYS","IND", "CON", "ISMS", "ORP", "OPS", "DER", "NET", "INF"]
 def readConfig(varname):
     configParser = configparser.RawConfigParser()
@@ -62,17 +66,18 @@ def doImport():
         site = Site.objects.get_current()
         for dirpath, dirnames, filenames in os.walk(settings.CRAWLER_DIRECTORY):
             # check the bsi article type is a component or threat or implementation notes
-            subArticleType = os.path.basename(dirpath)
-            if subArticleType == "C":
+            sub_article_type = os.path.basename(dirpath)
+            if sub_article_type == "C":
                 article_type = BSI_Article_type.COMPONENT
                 parent = BSI.get_or_create_bsi_subroots("components", "BSI.components", "", "Components")
-            elif subArticleType == "N":
+            elif sub_article_type == "N":
                 article_type = BSI_Article_type.IMPLEMENTATIONNOTES
                 parent = BSI.get_or_create_bsi_subroots("implementationNotes", "BSI.implementationNotes", "",
                                                         "Implementation Notes")
-            elif subArticleType == "T":
-                article_type = BSI_Article_type.THREAT
-                parent = BSI.get_or_create_bsi_subroots("threats", "BSI.threats", "", "Threats")
+            elif sub_article_type == "T":
+                 article_type = BSI_Article_type.THREAT
+                 parent = BSI.get_or_create_bsi_subroots("threats", "BSI.threats", "", "Threats")
+
             for filename in [f for f in filenames if f.endswith(".md")]:
                 # get the drive and the filepath
                 path_and_file = os.path.join(dirpath, filename)
@@ -80,9 +85,14 @@ def doImport():
                 location, file = os.path.split(path_and_file)
                 # get the file id and the titel
                 file_name = os.path.splitext(file)[0]
-                id = get_bsi_article_id(subArticleType, file_name)
+                id = get_bsi_article_id(sub_article_type, file_name)
 
-                # read md file content in variable
+                # append the Cross reference relation files to the content
+                # of each component article before import it in the database
+                if sub_article_type == "C":
+                    appendThreatMeasureRelation(path_and_file, id)
+
+                # import the content to the database
                 with open(path_and_file) as data_file:
                     content = data_file.readlines()
                     revision_kwargs = {'content': content, 'user_message': 'BSI article creation',
@@ -126,6 +136,7 @@ def find_between( s, first, last ):
 
 def get_bsi_article_id(type, file_name):
     # search the BSI id in the file name
+    id =''
     if type == 'C':
         id = file_name.split(" ",1)[0]
     elif type == "T":
@@ -165,9 +176,19 @@ def checkFileAction(filepath):
     return modified, added, deleted
 
 
-def appendThreatMeasureRelation(file):
-    # TODO
-    return
+def appendThreatMeasureRelation(path_and_file, id):
+    try:
+        for cr_file in [f for f in listdir(CRF) if f.endswith(".md")]:
+            path_and_ref = os.path.join(CRF, cr_file)
+            if id in cr_file:
+                with open(path_and_ref, 'r')as cr:
+                    cr_data = cr.read()
+                cr.close()
+                with open(path_and_file, 'a') as data_file:
+                    data_file.write(cr_data)
+                data_file.close()
+    except IOError:
+        print('An error occurred trying to open (read/write) the file.')
 
 
 #Calling Macro to get cross reference relation tables
@@ -185,10 +206,42 @@ def cleanUp():
     # We need the path to old BSI dir to update its content?
     return
 
+def get_cr_relation():
+    # extract for each component the cross reference relation
+    try:
+        for filename in [f for f in listdir(txtFolderDir) if f.endswith(".txt")]:
+            # extract the component, threats and requirements ids from the macro files
+            path_and_file = os.path.join(txtFolderDir, filename)
+            with open(path_and_file) as data_file:
+                componentId = data_file.readline().rstrip()
+                threats_requirements_id = data_file.read().splitlines()
+
+                # check the references folder (bsiCrawler) to append the ids
+                # to the names of the threats and requirements
+                # build new md files contains the CR relation
+                for referencename in [r for r in listdir(settings.REFERENCE_DIRECTORY) if r.endswith(".txt")]:
+                    if componentId in referencename:
+                        path_and_reference = os.path.join(settings.REFERENCE_DIRECTORY, referencename)
+                        reference_name = os.path.splitext(referencename)[0]
+                        component_file = CRF + reference_name
+                        new_component_file = open(component_file + '.md', "w+")
+
+                        for id in threats_requirements_id:
+                            with open(path_and_reference) as reference_file:
+                                 for line in reference_file:
+                                    if id.strip() in line:
+                                        line = line.replace("####", "  *")
+                                        new_component_file.write(line)
+                        new_component_file.close()
+    except IOError:
+        print('An error occurred trying to open (read/write) the file.')
+
 
 # should not be imported by other module
 if __name__ == '__main__':
     #generateComponentsThreatsMeasuresRelation()
+    #print( getCrossRefernceRelation())
+    #get_cr_relation()
     #bsiDir, file = parseArgs()
     #setdefault("DJANGO_SETTINGS_MODULE", "bsiwiki.settings")
     #django.setup()
