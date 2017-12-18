f663986140a374ded99a1224ac14388a4a29d216
EC2 Default User <ec2-user@ip-172-31-36-255.eu-central-1.compute.internal>
2017-12-18 14:46:29 +0000

    importer cleanup

--- django-wiki/Scripts/bsiImporter.py
+++ django-wiki/Scripts/bsiImporter.py
@@ -1,22 +1,21 @@
 import argparse
 import django
 import configparser
-import os
 import sys
-import string
-import filecmp
+from os.path import isdir, join, isfile, basename, split, splitext
+from os import listdir, environ, walk
 
 sys.path.append(r'..')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bsiwiki.settings")
+environ.setdefault("DJANGO_SETTINGS_MODULE", "bsiwiki.settings")
 django.setup()
-from os.path import  isfile, isdir, join
-from os import listdir
-from wiki.models import Article, URLPath, Site, ArticleRevision
+
 from bsiwiki import settings
-from bsi.models import BSI, UGA,BSI_Article_type
+from bsi.models import BSI, BSI_Article_type
 
 crfDir = './CRF/'
-system_devices = ["APP", "SYS","IND", "CON", "ISMS", "ORP", "OPS", "DER", "NET", "INF"]
+system_devices = ["APP", "SYS", "IND", "CON", "ISMS", "ORP", "OPS", "DER", "NET", "INF"]
+
+
 def readConfig(varname):
     configParser = configparser.RawConfigParser()
     configParser.read("config.cfg")
@@ -25,26 +24,22 @@ def readConfig(varname):
 
 def parseArgs():
     parser = argparse.ArgumentParser('Imports new or updated BSI contents to the database')
-    parser.add_argument('bsiDir', metavar='dir', help='The directory containing the BSI files to be imported')
     parser.add_argument('-u', '--update', help='Use this option only when it is an update, it should be followed by '
                         'the path to the text file containing the modified files')
 
     args = parser.parse_args()
-    if(not isdir(args.bsiDir)):
-        raise ValueError("Please give a valid directory path!")
-
     if(args.update is not None):
         if(not isfile(args.update)):
             raise ValueError("Please give a valid file path!")
         if(not args.update.endswith('txt')):
             raise ValueError("The file may only be a text file!")
 
-    return args.bsiDir, args.update
+    return args.update
 
 
-def main(bsiDir, update):
+def main(update):
     if(update):
-        doUpdate(bsiDir, update)
+        doUpdate(update)
     else:
         doImport()
 
@@ -52,58 +47,58 @@ def main(bsiDir, update):
 
 
 def doImport():
-    # go through the dir and read the content of each file
+        # go through the dir and read the content of each file
         # if it's a component, append the threat-measures relationships
         # just in case, look in DB, find if an article with the same headerID exists
         # if it doesn't (it should always be this case)
         # then create a new article and its urlpath
-        site = Site.objects.get_current()
-        for dirpath, dirnames, filenames in os.walk(settings.CRAWLER_DIRECTORY):
+        for dirpath, dirnames, filenames in walk(settings.CRAWLER_DIRECTORY):
             if not filenames:
                 continue
 
             # check the bsi article type is a component or threat or implementation notes
-            sub_article_type = os.path.basename(dirpath)
+            sub_article_type = basename(dirpath)
             if sub_article_type == "C":
                 article_type = BSI_Article_type.COMPONENT
-                parent = BSI.get_or_create_bsi_subroots("components", "BSI.components", "", "Components")
+                parent = BSI.get_or_create_bsi_subroots("components", "BSI.importer", "", "Components")
             elif sub_article_type == "N":
                 article_type = BSI_Article_type.IMPLEMENTATIONNOTES
-                parent = BSI.get_or_create_bsi_subroots("implementationNotes", "BSI.implementationNotes", "",
+                parent = BSI.get_or_create_bsi_subroots("implementationnotes", "BSI.importer", "",
                                                         "Implementation Notes")
             elif sub_article_type == "T":
-                 article_type = BSI_Article_type.THREAT
-                 parent = BSI.get_or_create_bsi_subroots("threats", "BSI.threats", "", "Threats")
+                article_type = BSI_Article_type.THREAT
+                parent = BSI.get_or_create_bsi_subroots("threats", "BSI.importer", "", "Threats")
 
             for filename in [f for f in filenames if f.endswith(".md")]:
                 # get the drive and the filepath
-                path_and_file = os.path.join(dirpath, filename)
+                path_and_file = join(dirpath, filename)
                 # get the path and file name
-                location, file = os.path.split(path_and_file)
+                location, file = split(path_and_file)
                 # get the file id and the titel
-                file_name = os.path.splitext(file)[0]
+                file_name = splitext(file)[0]
                 id = get_bsi_article_id(sub_article_type, file_name)
 
                 # append the Cross reference relation files to the content
                 # of each component article before import it in the database
-                if sub_article_type == "C" and os.path.isdir(crfDir):
+                if sub_article_type == "C" and isdir(crfDir):
                     appendThreatMeasureRelation(path_and_file, id)
 
                 # import the content to the database
                 with open(path_and_file) as data_file:
                     content = data_file.read()
-                    revision_kwargs = {'content': content, 'user_message': 'BSI article creation',
+                    revision_kwargs = {'content': content, 'user_message': 'BSI.importer',
                                        'ip_address': '0.0.0.0'}
                     BSI.create(parent=parent, slug=id, title=file_name, article_type=article_type, **revision_kwargs)
                     print(file_name + " is saved")
 
-def doUpdate(bsiDir, file):
+
+def doUpdate(file):
     # find out which files should be m/a/d
     modified, added, deleted = checkFileAction(file)
 
     # go through the dir and read the content of each file
-    for filename in [f for f in listdir(bsiDir) if f.lower().endswith('md')]:
-        fileContent = open(join(bsiDir, filename), "r").read()
+    for filename in [f for f in listdir(settings.CRAWLER_DIRECTORY) if f.lower().endswith('md')]:
+        fileContent = open(join(settings.CRAWLER_DIRECTORY, filename), "r").read()
 
         # if it's a component, append the threat-measures relationships
 
@@ -121,11 +116,12 @@ def doUpdate(bsiDir, file):
 
     return
 
-def find_between( s, first, last ):
+
+def find_between(s, first, last):
     # find the Implementation Notes id in the file name
     try:
         start = s.index(first)
-        end = s.index( last, start )
+        end = s.index(last, start)
         return s[start:end]
     except ValueError:
         return ""
@@ -133,18 +129,17 @@ def find_between( s, first, last ):
 
 def get_bsi_article_id(type, file_name):
     # search the BSI id in the file name
-    id =''
+    id = ''
     if type == 'C':
-        id = file_name.split(" ",1)[0]
+        id = file_name.split(" ", 1)[0]
     elif type == "T":
         id = "".join(file_name.split(" ", 2)[:2])
     elif type == "N":
-       for n_id in system_devices:
+        for n_id in system_devices:
             if n_id in file_name:
-                id = find_between(file_name, n_id," ")
+                id = find_between(file_name, n_id, " ")
 
     return id
-
 
 
 def checkFileAction(filepath):
@@ -176,7 +171,7 @@ def checkFileAction(filepath):
 def appendThreatMeasureRelation(path_and_file, id):
     try:
         for cr_file in [f for f in listdir(crfDir) if f.endswith(".md")]:
-            path_and_ref = os.path.join(crfDir, cr_file)
+            path_and_ref = join(crfDir, cr_file)
             if id in cr_file:
                 with open(path_and_ref, 'r')as cr:
                     cr_data = cr.read()
@@ -187,14 +182,16 @@ def appendThreatMeasureRelation(path_and_file, id):
     except IOError:
         print('An error occurred trying to open (read/write) the file.')
 
+
 def cleanUp():
     # TODO remove all temp dirs and update files in current dirs
     # We need the path to old BSI dir to update its content?
     return
 
+
 # should not be imported by other module
 if __name__ == '__main__':
-    #bsiDir, file = parseArgs()
-    #main(bsiDir, file)
-    doImport()
-    print("worked!")
+    file = parseArgs()
+    main(file)
+    print("finished!")
+
