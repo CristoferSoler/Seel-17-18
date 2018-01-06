824d637d7407b50e80d950b977bdbd1da0241058
SMILElikeJONNY <jonathan-zimmermann94@gmx.de>
2018-01-06 09:25:35 +0100

    added django app for the update mechanism

--- django-wiki/Scripts/demo_mid_phase.py
+++ django-wiki/Scripts/demo_mid_phase.py
@@ -2,7 +2,7 @@ from bsiImporter import mid_phase
 from sched import scheduler
 import time
 
-if __name__ == '__main__':
+def midPhase():
     scheduler = scheduler(time.time, time.sleep)
     print('Entering mid-phase...')
     scheduler.enter(0, 1, mid_phase, ())
