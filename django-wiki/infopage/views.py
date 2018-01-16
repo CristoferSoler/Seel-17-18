fe178726e3f71bdebc42ab3dafef615e3da2e837
Rima Faiqoh Augustine <augustine@campus.tu-berlin.de>
2018-01-17 00:03:52 +0100

    Minor edit + template information page in progress

--- django-wiki/infopage/views.py
+++ django-wiki/infopage/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect, Http404
 
-# Create your views here.
+
+def information(request):
+    return render(request, 'info.html')
