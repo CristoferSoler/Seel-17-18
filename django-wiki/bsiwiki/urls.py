de0208d5ff1c555a18758ee821e69d3bc634f995
SMILElikeJONNY <jonathan-zimmermann94@gmx.de>
2018-01-27 17:56:04 +0100

    #237 added boostrap for the forms

--- django-wiki/bsiwiki/urls.py
+++ django-wiki/bsiwiki/urls.py
@@ -20,6 +20,7 @@ from django_nyt.urls import get_pattern as get_nyt_pattern
 from wiki.urls import get_pattern
 from django.views.generic import TemplateView
 from bsi import views
+from bsi.forms import PasswordResetForm,SetPasswordForm
 
 
 admin.site.index_template = 'admin/index.html'
@@ -34,10 +35,10 @@ urlpatterns = [
         name='password_change'),
     url(r'^accounts/password/change/done/?', auth_views.password_change_done,
         {'template_name': 'bsi/account/passwordChangeDone.html'}, name='password_change_done'),
-    url(r'^password_reset/$', auth_views.password_reset,{'template_name': 'bsi/account/password_reset_form.html'}, name='password_reset'),
+    url(r'^password_reset/$', auth_views.password_reset,{'template_name': 'bsi/account/password_reset_form.html','password_reset_form': PasswordResetForm,'subject_template_name':'bsi/account/password_reset_subject.txt','email_template_name':'bsi/account/password_reset_email.html'}, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done,{'template_name': 'bsi/account/password_reset_done.html'}, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm,{'template_name': 'bsi/account/password_reset_confirm.html'}, name='password_reset_confirm'),
+        auth_views.password_reset_confirm,{'template_name': 'bsi/account/password_reset_confirm.html','set_password_form':SetPasswordForm}, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete,{'template_name': 'bsi/account/password_reset_complete.html'}, name='password_reset_complete'),
     url(r'^wizardinfo/?', TemplateView.as_view(template_name='wizard/helpWizard.html'),name = 'wizardInfo'),
     url(r'^_update/?', include('BSIUpdate.urls')),
