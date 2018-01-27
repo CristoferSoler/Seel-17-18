71ccb357a7d24aa5994734528d8c0096aeada4cb
TLara92 <lara.tunbari92@gmail.com>
2018-01-27 22:34:58 +0100

    #133 disable update btn during progress, add msg "update finished or already update" depending on the case of the update

--- django-wiki/BSIUpdate/update.py
+++ django-wiki/BSIUpdate/update.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 import json
 import sys
+from django.contrib import messages
+from django.http import Http404
 
 sys.path.append(r'..')
 
 from Scripts.main import update_phase#,demo_mid_phase, demo_post_phase
 
+update_done = dict(status='ok',msg='Update done! After 30 days will update the BSI version')
+error_update = dict(status='error',msg='Already updated')
 '''
 def performMidPhase(request):
     demo_mid_phase()
@@ -16,13 +20,22 @@ def performPostPhase(request):
     demo_post_phase()
     print('PostPhase')
     return HttpResponse('', content_type='application/json')
+
 '''
-def performUpdate(request):
-    update_phase()
-    print('Update')
-    return HttpResponse('', content_type='application/json')
 
+def performUpdate():
+    try:
+        update_phase()
+        return True
+    except:
+        return False
 
+def Update(request):
+    if(performUpdate()):
+        return HttpResponse(json.dumps(update_done), content_type='application/json')
+    else:
+        return HttpResponse(json.dumps(error_update), content_type='application/json')
+#
 '''
 def getTreeview(request):
     with open('./../programming/bsiCrawler/treeview/bsiTreeLinks.txt') as f:
